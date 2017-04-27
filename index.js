692841c1170066bf086b8e2f33bb09559c4a7c86
Joey Frohlinger <jmafrohlinger@outlook.com>
2017-04-26 22:29:16 -0700

    getting the auth code properly

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
+var secrets = require('./secrets');
+var format = require('string-format');
+var oauth_utils = require('./oauth-utils.js');
 
-app.get('/', function(req,res) {
+var oauthEndpoint = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={0}&scope=user.read&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2FauthODCallback';
+var callback = 'http://localhost/authODCallback'
+
+app.get('/', function (req, res) {
     res.send("hello world");
 });
 
+app.get('/authOD', function (req, res) {
+    res.redirect(format(oauthEndpoint, secrets.onedrive.id));
+});
 
-app.listen(80, function(){
+app.get('/authODCallback', function (req, res) {
+    oauth_utils.onedrive.GetToken(secrets.onedrive.id, callback, secrets.onedrive.secret, req.query.code);
+});
+
+app.listen(80, function () {
     console.log("express listening to port 80");
 });
---
+++ oauth-utils.js
@@ -0,0 +1,31 @@
+var Client = require('node-rest-client').Client;
+var quertystring = require('querystring');
+
+var client = new Client();
+
+client.registerMethod("oneDriveToken", "https://login.microsoftonline.com/common/oauth2/v2.0/token", "POST");
+
+
+
+module.exports = {
+    onedrive: {
+        GetToken: function (id, uri, secret, authCode) {
+            var args = {
+                headers: { "content-type": "application/x-www-form-urlencoded" },
+                data: {
+                    client_id: id,
+                    redirect_uri: uri,
+                    client_secret: secret,
+                    code: authCode,
+                    grant_type: 'authorization_code'
+                }
+            };
+
+            args.data = quertystring.stringify(args.data);
+            client.methods.oneDriveToken(args, function (data, response) {
+                console.log(data);
+                console.log(response);
+            });
+        }
+    }
+};
